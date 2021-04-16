b219db0ca0589815a2f91d27469f5809ed64e758
Patrick Lavin <plavin997@gmail.com>
2021-04-15 22:19:39 -0400

    first attemp at local isntall

---
+++ memtrace-to-npz/__init__.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def file_to_np(filename):
+    dt = np.dtype([('isWrite', '?'), ('pad', 'S7'), ('VA', 'u8'), ('size', 'u8'), ('IP', 'u8')])
+    data = np.fromfile(filename, dtype=dt)
+
+    isWrite = np.uint64(data['isWrite'])
+    VA      = np.uint64(data['VA'])
+    size    = np.uint64(data['size'])
+    IP      = np.uint64(data['IP'])
+
+    stacked = np.column_stack((isWrite, VA, size, IP))
+    return np.asarray(np.uint64(stacked))
+
+def file_to_npz(filename):
+    arr = file_to_np(filename)
+    np.savez_compressed('temp.npz', arr)
+
+#def file_to_csv(filename):
+#    arr = file_to_np(filename)
+#    np.savetxt('temp.csv', arr, delimiter=',', fmt='%d')
